3f95d545436e2f15333d9c7b4abd78151cd0e984
952kakaroto <alanmontalvo.333@gmail.com>
2023-06-29 08:35:58 -0600

    Proyecto_inicializado

---
+++ src/main/java/com/mycompany/los_chidos/LOS_CHIDOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.los_chidos;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class LOS_CHIDOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
