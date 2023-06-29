99585c4aa00589a514ea6bd778a84dcf4bf45b56
952kakaroto <alanmontalvo.333@gmail.com>
2023-06-29 09:00:51 -0600

    Implementando la clase producto

--- src/main/java/com/mycompany/los_chidos/model/ProductosModel.java
+++ src/main/java/com/mycompany/los_chidos/model/ProductosModel.java
@@ -9,5 +9,57 @@ package com.mycompany.los_chidos.model;
  * @author LENOVO
  */
 public class ProductosModel {
+     
+    private String nombre;
+    private double precio;
+    private Integer Cantidad;
+    private String Descripcion;
+
+    public ProductosModel(String nombre, double precio, Integer Cantidad, String Descripcion) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.Cantidad = Cantidad;
+        this.Descripcion = Descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return Cantidad;
+    }
+
+    public void setCantidad(Integer Cantidad) {
+        this.Cantidad = Cantidad;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductosModel{" + "nombre=" + nombre + ", precio=" + precio + ", Cantidad=" + Cantidad + ", Descripcion=" + Descripcion + '}';
+    }
+
+   
+    
     
 }
